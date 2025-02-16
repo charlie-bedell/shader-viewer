6a678ae5ce9522e2ebb08873c1f4624bcc2ed02d
Charles Bedell <charlesbedell5@gmail.com>
2025-02-15 19:04:50 -0500

    load shader and pass uniforms, draw to screen

---
+++ shaders/template.fs
@@ -0,0 +1,14 @@
+#version 330 core
+
+uniform float uTime;
+uniform vec2 uResolution;
+
+out vec4 FragColor;
+
+void main() {
+    vec2 uv = gl_FragCoord.xy / uResolution.xy;
+    float r = 0.5 + 0.5 * sin(uTime + uv.x * 10.0);
+    float g = 0.5 + 0.5 * sin(uTime + uv.y * 10.0);
+    float b = 0.5 + 0.5 * sin(uTime);
+    FragColor = vec4(r, g, b, 1.0);
+}
--- src/main.c
+++ src/main.c
@@ -1,16 +1,57 @@
 #include <stdio.h>
 #include "raylib.h"
+#include "rlgl.h"
+
+#if defined(PLATFORM_DESKTOP)
+  #define GLSL_VERSION 330
+#else // PLATFORM_ANDROID, PLATFORM_WEB
+  #define GLSL_VERSION 100
+#endif
+
+#define WINDOW_W 800
+#define WINDOW_H 800
+
 
 int main(void) {
-	printf("hello, world!");
+
+	InitWindow(WINDOW_W, WINDOW_H, "noyabai window");
+
+	Shader shader = LoadShader(0, "shaders/template.fs");
+	if (shader.id == 0) {printf("shader failed to load!\n"); return 1;}
 	
-	InitWindow(500, 500, "noyabai window");
+	int uTimeLoc = GetShaderLocation(shader, "uTime");
+	int uResolutionLoc = GetShaderLocation(shader, "uResolution");
+	if (uTimeLoc == -1 || uResolutionLoc == -1) {printf("unable to find shader uniforms!\n");return 1;}
+
+	float time = 0.0f;
+	Rectangle rect = {25, 25, 750, 750};
+	Vector2 resolution = {WINDOW_W, WINDOW_H};
+
+	SetShaderValue(shader, uTimeLoc, &time, SHADER_UNIFORM_FLOAT);
+	SetShaderValue(shader, uResolutionLoc, &resolution, SHADER_UNIFORM_VEC2);
+
 	SetTargetFPS(60);
+
 	while (!WindowShouldClose()) {
+		// UPDATE
+		time = (float)GetTime();
+		SetShaderValue(shader, uTimeLoc, &time, SHADER_UNIFORM_FLOAT);
+		SetShaderValue(shader, uResolutionLoc, &resolution, SHADER_UNIFORM_VEC2);
+		// END UPDATE
+
+		// DRAW
 		BeginDrawing();
-		ClearBackground(WHITE);
-		DrawText("Hello, world!", 50, 100, 20, BLACK);
+
+		ClearBackground(BLACK);
+		BeginShaderMode(shader);
+		DrawRectangleRec(rect, WHITE);
+		EndShaderMode();
+
 		EndDrawing();
+		// END DRAW
 	}
+	UnloadShader(shader);
 	CloseWindow();
+
+	return 0;
 }
